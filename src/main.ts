059e112bc003363c7e43cd212495556d8d24b0a7
Klavdija <klavdija@pop-os.localdomain>
2020-01-04 13:22:46 +0100

    consul

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import './config';
+
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('v1');
+  await app.listen(process.env.PORT);
 }
 bootstrap();

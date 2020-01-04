36e4318a7b971a3a5295670baafb46af8ece258a
Klavdija <klavdija@pop-os.localdomain>
2020-01-04 13:59:02 +0100

    image upload

--- src/upload/upload.controller.ts
+++ src/upload/upload.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+import { UploadService } from './upload.service';
 
 @Controller('upload')
-export class UploadController {}
+export class UploadController {
+
+    constructor(private uploadService: UploadService) {
+
+    }
+
+    @Post()
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadFile(@UploadedFile() file) {
+        return await this.uploadService.saveFile(file);
+    }
+}
--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { ConsulConfig, InjectConfig } from '@nestcloud/config';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { S3 } from 'aws-sdk';
+import { createHash } from 'crypto';
+import { extname } from 'path';
+import * as sharp from 'sharp';
+
+function randomSHA1Hash() {
+  return createHash('sha1')
+    .update(Date.now() + '' + Math.random())
+    .digest('hex');
+}
 
 @Injectable()
-export class UploadService {}
+export class UploadService {
+  private s3: S3;
+
+  constructor(
+    @InjectConfig() private config: ConsulConfig,
+  ) {
+    config.watch('aws', data => {
+      this.s3 = new S3(data);
+    });
+  }
+
+  async saveFile(file) {
+    if (!file) {
+      throw new BadRequestException("File is missing!");
+    }
+
+    const uploadStream = this.s3.upload({
+      Bucket: this.config.get('aws.bucket'),
+      Key: randomSHA1Hash() + extname(file.originalname).toLowerCase(),
+      ACL: 'public-read',
+      Body: await sharp(file.buffer)
+        .resize(512, 512, { withoutEnlargement: true })
+        .toBuffer()
+    });
+
+    const response = await uploadStream.promise();
+
+    return { imageUrl: response.Location }
+
+  }
+}
